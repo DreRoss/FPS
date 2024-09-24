d97f85655397bfaf3b2c647daa7bb986bd2daa02
DreRoss <androssini1997@gmail.com>
2024-09-24 16:01:21 +0200

    Creazione inner class FSavedMove_FPS, dichiarazione metodi CanCombineWith(), Clear(), GetCompressedFlags, setMoveFor(). PrepMoveFor()

--- FPS/Public/FPSCharacterMovementComponent.h
+++ FPS/Public/FPSCharacterMovementComponent.h
@@ -10,6 +10,19 @@ class FPS_API UFPSCharacterMovementComponent : public UCharacterMovementComponen
 {
 	GENERATED_BODY()
 
+	class FSavedMove_FPS : public FSavedMove_Character
+	{
+		typedef FSavedMove_Character Super;
+
+		uint8 Saved_bWantsToSprint:1;
+
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		virtual void Clear() override;
+		virtual uint8 GetCompressedFlags() const override;
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
+
 	bool Safe_bWantsToSprint;
 	
 	public:
