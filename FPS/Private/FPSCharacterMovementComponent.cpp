a427ae9e2a76ca23858c9db803366bacb5543940
DreRoss <androssini1997@gmail.com>
2024-09-27 11:31:18 +0200

    Implementazione metodi CanCombineWith(), Clear(), GetCompressedFlags, setMoveFor(). PrepMoveFor() in FPSCMC.cpp

--- FPS/Private/FPSCharacterMovementComponent.cpp
+++ FPS/Private/FPSCharacterMovementComponent.cpp
@@ -1,1 +1,50 @@
 ﻿#include "FPSCharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+
+bool UFPSCharacterMovementComponent::FSavedMove_FPS::CanCombineWith(const FSavedMovePtr& NewMove,
+                                                                    ACharacter* InCharacter, float MaxDelta) const
+{
+	FSavedMove_FPS* NewFPSMove = static_cast<FSavedMove_FPS*>(NewMove.Get());
+
+	if(Saved_bWantsToSprint != NewFPSMove->Saved_bWantsToSprint)
+	{
+		return false;
+	}
+	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
+		
+}
+
+void UFPSCharacterMovementComponent::FSavedMove_FPS::Clear()
+{
+	FSavedMove_Character::Clear();
+
+	Saved_bWantsToSprint = 0;
+}
+
+uint8 UFPSCharacterMovementComponent::FSavedMove_FPS::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if(Saved_bWantsToSprint) Result |= FLAG_Custom_0;
+
+	return Result;
+}
+
+void UFPSCharacterMovementComponent::FSavedMove_FPS::SetMoveFor(ACharacter* C, float InDeltaTime,
+	FVector const& NewAccel, class FNetworkPredictionData_Client_Character& ClientData)
+{
+	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+
+	UFPSCharacterMovementComponent* CharacterMovement = Cast<UFPSCharacterMovementComponent>(C->GetCharacterMovement());
+
+	Saved_bWantsToSprint = CharacterMovement->Safe_bWantsToSprint;
+}
+
+void UFPSCharacterMovementComponent::FSavedMove_FPS::PrepMoveFor(ACharacter* C)
+{
+	FSavedMove_Character::PrepMoveFor(C);
+
+	UFPSCharacterMovementComponent* CharacterMovement = Cast<UFPSCharacterMovementComponent>(C->GetCharacterMovement());
+
+	CharacterMovement->Safe_bWantsToSprint = Saved_bWantsToSprint;
+}
